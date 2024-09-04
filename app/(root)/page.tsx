946009230e662b5a23fd86db60c753835c42b4d6
Noah (Pham Tran Khoi Nguyen) <ptknguyen04@gmail.com>
2024-09-04 18:30:17 +0100

    Added Total Balance Box and Header Box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client";
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const DashBoard = () => {
+  const loggedIn = { firstName: 'Noah' };
+
   return (
-    <div>DashBoard</div>
-  )
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+            <HeaderBox 
+              type="greeting"
+              title="Welcome"
+              user={loggedIn?.firstName || 'Guest'}
+              subtext="Access and manage your account and transactions efficiently."
+            />
+
+            <TotalBalanceBox
+              accounts={[]}
+              totalBanks={1}
+              totalCurrentBalance={9999.99}
+            />
+        </header>
+      </div>
+    </section>
+  );
 }
 
-export default DashBoard
+export default DashBoard;
