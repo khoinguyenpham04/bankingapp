a2e43f547063db2fa9bcb7e64271486f52db3d7e
Noah (Pham Tran Khoi Nguyen) <ptknguyen04@gmail.com>
2024-10-05 16:33:27 +0100

    added mobile nav and sheet with shadcn

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from 'next/image';
+  
 
 const MobileNav = ({ user }:MobileNavProps) => {
   return (
-    <div>MobileNav</div>
+    <section className="w-full max-w-[264px]">
+        <Sheet>
+            <SheetTrigger>
+                <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu" className="cursor-pointer" />
+            </SheetTrigger>
+            <SheetContent side="left">
+                
+            </SheetContent>
+        </Sheet>
+    </section>
   )
 }
 
